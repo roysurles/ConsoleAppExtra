abea9c176d28af0bc7d66a443586688bd0c48c93
Roy Surles <surlesr@earthlink.net>
2023-08-23 09:18:50 -0500

    Update to .net 7

--- src/ConsoleAppExtra/Program.cs
+++ src/ConsoleAppExtra/Program.cs
@@ -11,9 +11,10 @@ namespace ConsoleAppExtra;
 // Serilog
 // parameter (args) checking
 
+// Best Practices - https://benfoster.io/blog/serilog-best-practices/
+
 internal class Program
 {
-
     static IHostEnvironment HostEnvironment { get; set; } = null!;
 
     /// <summary>
@@ -24,13 +25,26 @@ internal class Program
     static void Main(string[] args)
     {
         // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-6.0
+        // https://github.com/serilog/serilog/wiki/Configuration-Basics
+
+        // NOTE:  serilog only works in catch & finally blocks if next 3 lines are outside of try / catch / finnally
+        // TODO:  investigate how to get serilog working w/o CreateHostBuilder so that we can log any exception that occurs in CreateHostBuilder
+        /*
+         * https://docs.datalust.co/docs/using-serilog
+         * var auditLogger = new LoggerConfiguration()
+                            .AuditTo.Seq("https://seq.example.com")
+                            .CreateLogger();
+         */
+        using IHost host = CreateHostBuilder(args);
+        Configuration = host.Services.GetRequiredService<IConfiguration>();
+        CreateSerilogLogger();
         try
         {
-            using IHost host = CreateHostBuilder(args);
-            Configuration = host.Services.GetRequiredService<IConfiguration>();
-            CreateSerilogLogger();
+            //using IHost host = CreateHostBuilder(args);
+            //Configuration = host.Services.GetRequiredService<IConfiguration>();
+            //CreateSerilogLogger();
 
-            Log.Information("App Starting...");
+            Log.Information("{V}", $"App Starting...{Environment.NewLine}");
 
             var programLogger = Log.Logger.ForContext<Program>();
             programLogger.Information("Log message for Program.cs");
@@ -50,11 +64,11 @@ internal class Program
         }
         catch (Exception ex)
         {
-            Log.Error(ex, "Error occurred: ");
+            Log.Fatal(ex, "Exception occurred: ");
         }
         finally
         {
-            Log.Information("{V}", $"App Exiting...{Environment.NewLine}");
+            Log.Information("{V}", $"App Exiting{Environment.NewLine}");
             Log.CloseAndFlush();
         }
     }
@@ -65,8 +79,10 @@ internal class Program
         return Host.CreateDefaultBuilder(args)
                 .ConfigureServices((_, services) =>
                 {
+                    //services.AddLogging(logging => logging.AddSerilog(dispose: true));
                     services.AddTransient<IWorkerService, WorkerService>();
                 })
+                .UseSerilog()
                 .ConfigureAppConfiguration((hostingContext, configuration) =>
                 {
                     configuration.Sources.Clear();
@@ -102,14 +118,16 @@ internal class Program
         //    .AddJsonFile(path: "appsettings.development.json", optional: false, reloadOnChange: true)
         //    .Build();
 
-        /// push the templates to appsettings
+        /// TODO: push the templates to appsettings
+        /// https://procodeguide.com/programming/aspnet-core-logging-with-serilog/
         const string OutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}{Properties}";
         const string OutputTemplate1 = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Properties:j} {Message:lj}{NewLine}{Exception}";
 
+        // https://rmauro.dev/setup-serilog-in-net6-as-logging-provider/
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(Configuration)
-            .Enrich.FromLogContext()
             .Enrich.WithMachineName()
+            .Enrich.FromLogContext()
             .WriteTo.Console(outputTemplate: OutputTemplate1)
             .WriteTo.Debug(outputTemplate: OutputTemplate1)
             .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day, outputTemplate: OutputTemplate1)
